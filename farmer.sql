e9588c7d1bf604b0ab226ba92ce9d4261aebe64b
Mike Yang <18604713262@163.com>
2021-08-15 20:41:30 -0400

    asd

--- farmer.sql
+++ farmer.sql
@@ -4,9 +4,9 @@ USE farmerdata;
 
 FLUSH privileges;
 
--- SHOW GLOBAL VARIABLES LIKE 'local_infile';
--- SET GLOBAL local_infile = 'ON';
--- SHOW GLOBAL VARIABLES LIKE 'local_infile';
+SHOW GLOBAL VARIABLES LIKE 'local_infile';
+SET GLOBAL local_infile = 'ON';
+SHOW GLOBAL VARIABLES LIKE 'local_infile';
 
 USE farmerdata;
 
@@ -69,17 +69,17 @@ CREATE TABLE `farmers` (
   `petfood` BOOLEAN DEFAULT NULL,
   `tofu` BOOLEAN DEFAULT NULL,
   `wildharvested` BOOLEAN DEFAULT NULL,
-  `updateTime` VARCHAR(255) DEFAULT NULL
+  `updatetime` VARCHAR(255) DEFAULT NULL
 );
 -- ["SET "+part1[i]+" = (@var"+str(i+1)+" = \'Y\');\n" for i in range(len(part1))]
 SET GLOBAL local_infile = 'ON';
 
-LOAD DATA INFILE '/opt/tomcat/apache-tomcat-9.0.52/webapps/farmer/Export.csv' INTO TABLE airlines
+LOAD DATA LOCAL INFILE '/home/laptopadmin/Downloads/Export.csv' INTO TABLE farmers 
 FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
 LINES TERMINATED BY '\n' (id, name,
   website,facebook,twitter,youtube,othermedia,street,city,county,state,zip,season1date,season1time,season2date,season2time,season3date,season3time,season4date,season4time,x,y,location,credit,wic,wiccash,sfmnp,snap
 , @var1, @var2, @var3, @var4, @var5, @var6, @var7, @var8, @var9, @var10, @var11, @var12, @var13, @var14, @var15, @var16, @var17, @var18, @var19, @var20, @var21, @var22, @var23, @var24, @var25, @var26, @var27, @var28, @var29, @var30
-)
+,updatetime)
 SET organic = (@var1 = 'Y');
 SET bakedgoods = (@var2 = 'Y');
 SET cheese = (@var3 = 'Y');
