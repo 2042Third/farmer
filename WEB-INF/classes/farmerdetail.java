74daf9c7443d462acaf7551fc69c70f7043ca47f
Mike Yang <18604713262@163.com>
2021-08-17 12:58:55 -0400

    script commit

--- WEB-INF/classes/farmerdetail.java
+++ WEB-INF/classes/farmerdetail.java
@@ -83,7 +83,7 @@ public class farmerdetail extends HttpServlet {
           "  background-color: #dddddd;\r\n" + 
           "}\r\n" + 
           "</style><script type=\"text/javascript\">var x = document.getElementById(\"geo\"); function getLocation() { if (navigator.geolocation) { navigator.geolocation.getCurrentPosition(showPosition, showError); } else { x.innerHTML = \"Geolocation is not supported by this browser.\"; }} function showPosition(position) { x.innerHTML = \"Latitude: \" + position.coords.latitude + \"<br>Longitude: \" + position.coords.longitude;}function showError(error) { switch(error.code) { case error.PERMISSION_DENIED: x.innerHTML = \"User denied the request for Geolocation.\" break; case error.POSITION_UNAVAILABLE: x.innerHTML = \"Location information is unavailable.\" break; case error.TIMEOUT: x.innerHTML = \"The request to get user location timed out.\" break; case error.UNKNOWN_ERROR: x.innerHTML = \"An unknown error occurred.\" break; }}</script></head>"  );
-    response.getWriter().append("<h2>Geolocation</h2><p><button onclick=\"getLocation()\" class=\"primary\">Get My Location</button> </p>");
+    response.getWriter().append("<h2>Geolocation</h2><p><button onclick=\"getLocation()\" class=\"primary\">Get My Location</button> </p> <p id=geo></p>");
     get_textarea(id,message,user_name,response);
     get_review(id,message,user_name,response);
     get_from_sql(id, response);
