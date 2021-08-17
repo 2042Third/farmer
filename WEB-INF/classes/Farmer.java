296915cfd247dde41424d66744f57618e2b6bc7c
Mike Yang <18604713262@163.com>
2021-08-17 14:58:24 -0400

    script commit

--- WEB-INF/classes/Farmer.java
+++ WEB-INF/classes/Farmer.java
@@ -119,7 +119,7 @@ public class Farmer extends HttpServlet {
       try
       {
         con = DriverManager.getConnection(url, "admin", "f3ck");
-        String query = "SELECT id, name, website,city, county, state, reviewcount, reviewscore " + 
+        String query = "SELECT id, name, website,city, county, state, reviewcount, reviewscore , x ,y" + 
                 "FROM farmerdata.farmers WHERE city LIKE ? AND state LIKE ? LIMIT 20 OFFSET ? ;";
         resultTable = new StringBuffer("<table>"+
         "<tr><th>Name</th><th>City/County</th><th>State</th>" +
@@ -138,6 +138,9 @@ public class Farmer extends HttpServlet {
                 append("</td><td>").append(rs.getString("reviewscore")+"/5 ("+rs.getString("reviewcount")+")").
                 append("</td><td>").append(rs.getString("website")).
                 append("</td><td>").append("<a href=\"/farmer/farmerdetail?id="+rs.getString("id")+"\" >detail</a>").
+                append("</td><td style=\"display:none;\">").append(rs.getString("x")).
+                append("</td><td style=\"display:none;\">").append(rs.getString("y")).
+
                 append("</td></tr>");
             }
           }
